39a364bb6263d93e86b32eb9f7f3c4343b09d7d5
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2022-10-10 19:30:16 +0530

    final commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -42,51 +42,51 @@ function Calculator() {
     }
     return (
         <div className="calculator">
-        <div id="display-container">
-            <input name="" value={question} readOnly />
+            <div id="display-container">
+                <input name="" value={question} readOnly />
+            </div>
+            <div className="button-container">
+                
+                <button value='clear' onClick={handleClick}>AC</button>
+                
+                <button value='sign' onClick={handleClick}>+/-</button>
+                
+                <button value='%' onClick={handleClick}>%</button>
+                
+                <button className="red" value='/' onClick={handleClick}>/</button>
+
+                <button value='7' onClick={handleClick}>7</button>
+
+                <button value='8' onClick={handleClick}>8</button>
+
+                <button value='9' onClick={handleClick}>9</button>
+
+                <button className="red" value='*' onClick={handleClick}>*</button>
+
+                <button value='4' onClick={handleClick}>4</button>
+
+                <button value='5' onClick={handleClick}>5</button>
+
+                <button value='6' onClick={handleClick}>6</button>
+
+                <button className="red" value='-' onClick={handleClick}>-</button>
+
+                <button value='1' onClick={handleClick}>1</button>
+
+                <button value='2' onClick={handleClick}>2</button>
+
+                <button  value='3' onClick={handleClick}>3</button>
+
+                <button className="red" value='+' onClick={handleClick}>+</button>
+
+                <button id="button-zero" value='0' onClick={handleClick}>0</button>
+
+                <button value='.' onClick={handleClick}>.</button>
+
+                <button className="red" value='=' onClick={handleClick}>=</button>
+
+            </div>
         </div>
-        <div className="button-container">
-            
-            <button value='clear' onClick={handleClick}>AC</button>
-            
-            <button value='sign' onClick={handleClick}>+/-</button>
-            
-            <button value='%' onClick={handleClick}>%</button>
-            
-            <button className="red" value='/' onClick={handleClick}>/</button>
-
-            <button value='7' onClick={handleClick}>7</button>
-
-            <button value='8' onClick={handleClick}>8</button>
-
-            <button value='9' onClick={handleClick}>9</button>
-
-            <button className="red" value='*' onClick={handleClick}>*</button>
-
-            <button value='4' onClick={handleClick}>4</button>
-
-            <button value='5' onClick={handleClick}>5</button>
-
-            <button value='6' onClick={handleClick}>6</button>
-
-            <button className="red" value='-' onClick={handleClick}>-</button>
-
-            <button value='1' onClick={handleClick}>1</button>
-
-            <button value='2' onClick={handleClick}>2</button>
-
-            <button  value='3' onClick={handleClick}>3</button>
-
-            <button className="red" value='+' onClick={handleClick}>+</button>
-
-            <button id="button-zero" value='0' onClick={handleClick}>0</button>
-
-            <button value='.' onClick={handleClick}>.</button>
-
-            <button className="red" value='=' onClick={handleClick}>=</button>
-
-        </div>
-    </div>
     );
   }
   
